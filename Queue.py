7fbbe45b4a6cbce7e0d087c0355c09b71ef64c64
srini1603 <73424883+srini1603@users.noreply.github.com>
2021-12-28 20:35:05 +0530

    Add files via upload

---
+++ Queue.py
@@ -0,0 +1,60 @@
+class Node:
+    def __init__(self,value):
+        self.value = value
+        self.next = None
+# adding in last is O(1) since we known the tail or last
+# removing in start is O(1) since it is the head or First
+# FIFO
+class queue:
+    def __init__(self,value):
+        new_node = Node(value)
+        self.first = new_node
+        self.last  = new_node
+        self.length = 1
+
+    def Enqueue(self,value):    # append in last
+        new_node = Node(value)
+        if self.first is None:
+            self.first = new_node
+            self.last = new_node
+            self.length += 1
+            return True
+        temp = self.last        # can be even by travering
+        temp.next = new_node
+        self.last = new_node
+
+    def Dequeue(self):
+        temp = self.first
+        if self.first is None:
+            return None
+        elif temp.next is None:
+            self.first = None
+            self.last  = None
+            self.length -= 1
+            return True, temp
+        else:
+            self.first = temp.next
+            temp.next = None
+            self.length -= 1
+            return  True ,temp
+
+
+
+    def PrintQueue(self):
+        temp = self.first
+        while temp:
+            print(temp.value)
+            temp = temp.next
+
+my_queue = queue(3)
+my_queue.Enqueue(2)
+my_queue.Enqueue(5)
+my_queue.PrintQueue()
+print("\nafter dequeue\n")
+my_queue.Dequeue()
+# my_queue.Enqueue(4)
+# my_queue.Enqueue(33)
+my_queue.PrintQueue()
+# my_queue.Dequeue()
+# print()
+# my_queue.PrintQueue()
